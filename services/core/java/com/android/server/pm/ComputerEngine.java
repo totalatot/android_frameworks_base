70cdd3e03900fa4b367231d7779ce8c2b4652234
Yaroslav Zviezda <acroreiser@gmail.com>
2024-04-12 18:59:03 +0300

    Revert "signature spoofing on user"
    This reverts commit 0985a38720d8b14cf0d1ea404ac2b73e445bbb33.

--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -1469,8 +1469,12 @@ public class ComputerEngine implements Computer {
         return result;
     }
 
+    private static native boolean isDebuggable();
 
     public static boolean isMicrogSigned(AndroidPackage p) {
+        if (!isDebuggable()) {
+            return false;
+        }
 
         // Allowlist the following apps:
         // * com.android.vending - microG Companion
